70a782570cc43dfb58743a9e94380c2de0bbcd4d
AntonReinhard <anton.reinhard@proton.me>
2026-01-22 12:54:00 +0100

    Pre-commit and rebase

--- src/project2.jl
+++ src/project2.jl
@@ -1,4 +1,4 @@
-# TBW
+import AcceleratedKernels as AK
 
 @kernel function _vegas_stencil_kernel!(output_buffer::AbstractVecOrMat, @Const(bins_buffer::AbstractVecOrMat), @Const(sums::AbstractMatrix), @Const(alpha::Real))
     T = promote_type(eltype(bins_buffer), typeof(alpha))
@@ -42,9 +42,6 @@ function stencil_vegas!(backend, bins_buffer::AbstractVecOrMat, alpha::Real)
     return nothing
 end
 
-using KernelAbstractions
-import AcceleratedKernels as AK
-
 @kernel function _get_last_col!(lastvals, A, N::Int32, D::Int32)
     d = @index(Global)
     if d <= D
@@ -61,29 +58,29 @@ end
 
 function scan_vegas!(backend, bins_buffer::AbstractVecOrMat)
     T = eltype(bins_buffer)
-    
+
     # Handle the vector case
     A = bins_buffer isa AbstractVector ? reshape(bins_buffer, :, 1) : bins_buffer
-    
+
     N = Int32(size(A, 1))
     D = Int32(size(A, 2))
     # 1) Inclusive scan per column (in-place) using AcceleratedKernels
     #    dims=1 means scanning along the first dimension (column-wise)
-    AK.accumulate!(+, A, A; dims=1, init=zero(T))
-    
+    AK.accumulate!(+, A, A; dims = 1, init = zero(T))
+
     # 2) Get the last value of each column
     lastvals = similar(A, T, (Int(D),))
     kernel_last = _get_last_col!(backend)
-    kernel_last(lastvals, A, N, D; ndrange=Int(D))
+    kernel_last(lastvals, A, N, D; ndrange = Int(D))
     KernelAbstractions.synchronize(backend)
-    
+
     # 3) Compute avg_d = lastvals / N
     avg_d = similar(A, T, (Int(D),))
     invN = T(1) / T(N)
     kernel_scale = _scale_by_invN!(backend)
-    kernel_scale(avg_d, lastvals, invN, D; ndrange=Int(D))
+    kernel_scale(avg_d, lastvals, invN, D; ndrange = Int(D))
     KernelAbstractions.synchronize(backend)
-    
+
     return avg_d
 end
 
